381b4ae0eac0e99b6f04688fdbbda6071d585f89
Isajk2 <63793706+Isajk2@users.noreply.github.com>
2023-12-08 08:46:57 +0100

    tror det är klart nu

--- scriptCecilia.js
+++ scriptCecilia.js
@@ -5,19 +5,78 @@ const myButton = document.getElementById('button');
 const myOutput = document.getElementById('output');
 const myBox = document.querySelector('.box');
 
+// Lyssnare för knappklick
 myButton.addEventListener('click', function() {
+    // Ta bort utdatasektionen
     myOutput.remove();
 });
 
+// Lyssnare för ändring i checkbox
 myCheckbox.addEventListener('change', function() {
+    // Anropa funktion för att ändra bakgrundsfärg
     changeBackgroundColor();
 });
 
+// Lyssnare för inmatning i innehållsfältet
 myTextfieldContent.addEventListener('input', function() {
+    // Uppdatera utdatasektionen med inmatat värde
     myOutput.innerHTML = myTextfieldContent.value;
 });
 
+// Funktion för att ändra bakgrundsfärg baserat på färginmatning
 function changeBackgroundColor() {
+    // Hämta färgvärdet från färginmatningen
     const colorValue = myTextfieldColor.value;
+    // Tillämpa färgen på box-elementet
     myBox.style.backgroundColor = colorValue;
+}
+
+
+
+
+
+// LEK
+
+
+// Lyssnare för knappklick
+myButton.addEventListener('click', function() {
+    // Ta bort utdatasektionen
+    myOutput.remove();
+});
+
+// Lyssnare för ändring i checkbox
+myCheckbox.addEventListener('change', function() {
+    // Anropa funktion för att ändra bakgrundsfärg
+    changeBackgroundColor();
+});
+
+// Lyssnare för inmatning i innehållsfältet
+myTextfieldContent.addEventListener('input', function() {
+    // Uppdatera utdatasektionen med inmatat värde
+    myOutput.innerHTML = myTextfieldContent.value;
+});
+
+// Lyssnare för tangenttryckning i innehållsfältet
+myTextfieldContent.addEventListener('keyup', function(event) {
+    // Om Enter-tangenten trycks ned (key code 13)
+    if (event.keyCode === 13) {
+        // Visa boxen igen med det nya meddelandet
+        showBox();
+    }
+});
+
+// Funktion för att ändra bakgrundsfärg baserat på färginmatning
+function changeBackgroundColor() {
+    // Hämta färgvärdet från färginmatningen
+    const colorValue = myTextfieldColor.value;
+    // Tillämpa färgen på box-elementet
+    myBox.style.backgroundColor = colorValue;
+}
+
+// Funktion för att visa boxen
+function showBox() {
+    // Återskapa utdatasektionen om den tidigare togs bort
+    if (!myOutput.parentElement) {
+        myBox.appendChild(myOutput);
+    }
 }
